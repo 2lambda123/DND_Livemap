c8b4fcf5736b01101530725dddc3b6bd840852cf
TheOneTrueNoodle <72693746+TheOneTrueNoodle@users.noreply.github.com>
2023-07-09 15:48:15 +0100

    Made unity project and camera controls

---
+++ DND_Livemap/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraControl : MonoBehaviour
+{
+    private Vector3 Origin;
+    private Vector3 Difference;
+    private Vector3 ResetCamera;
+
+    private CinemachineVirtualCamera virtualCam;
+    private Camera cam;
+
+    private bool drag = false;
+
+    public float ZoomChange;
+    public float SmoothChange;
+    public float MinSize, MaxSize;
+
+    private void Start()
+    {
+        cam = Camera.main;
+        virtualCam = GetComponent<CinemachineVirtualCamera>();
+        ResetCamera = Camera.main.transform.position;
+    }
+
+    private void Update()
+    {
+        if (Input.mouseScrollDelta.y > 0)
+        {
+            virtualCam.m_Lens.OrthographicSize -= ZoomChange * Time.deltaTime * SmoothChange;
+        }
+        if (Input.mouseScrollDelta.y < 0)
+        {
+            virtualCam.m_Lens.OrthographicSize += ZoomChange * Time.deltaTime * SmoothChange;
+        }
+
+        virtualCam.m_Lens.OrthographicSize = Mathf.Clamp(virtualCam.m_Lens.OrthographicSize, MinSize, MaxSize);
+    }
+
+    private void LateUpdate()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            Difference = cam.ScreenToWorldPoint(Input.mousePosition) - cam.transform.position;
+            if (drag == false)
+            {
+                drag = true;
+                Origin = cam.ScreenToWorldPoint(Input.mousePosition);
+            }
+        }
+        else
+        {
+            drag = false;
+        }
+
+        if (drag)
+        {
+            virtualCam.transform.position = Origin - Difference;
+        }
+    }
+}
